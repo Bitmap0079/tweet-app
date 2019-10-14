bb3bdafb266d4e52207b6934dbbccd52acae36b9
Tomoyuki Oka <tomochiki00@yahoo.co.jp>
2019-10-14 08:34:10 +0000

    Add New function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  get 'home/top'
-
+  post 'posts/create' => "posts#create"
+  
+  get 'posts/index' => "posts#index"
+  get 'posts/new' => "posts#new"
+  get '/posts/:id' => "posts#show"
+  get '/' => "home#top"
+  get 'about' => "home#about"
+  get 'posts/index' => "posts#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
