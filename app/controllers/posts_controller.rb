cbb05d98573742c173efd8bf56ac07ddb2259804
Tomoyuki Oka <tomochiki00@yahoo.co.jp>
2019-10-24 08:25:19 +0000

    Fixed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,8 +21,8 @@ class PostsController < ApplicationController
       user_id: @current_user.id
       )
     if @post.save
-      redirect_to "/posts/index"
       flash[:notice] = "投稿が作成されました"
+      redirect_to "/posts/index"
     else
       render "posts/new"
     end
