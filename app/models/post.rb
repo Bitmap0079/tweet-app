db9e06f8bddd9adc38e0709b0b3dbcd488d8d2fe
Tomoyuki Oka <tomochiki00@yahoo.co.jp>
2019-10-28 17:27:01 +0000

    Fix

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   validates :content, {presence: true, length: {maximum: 140}}
   validates :user_id, {presence: true}
+  validates :user_name, {presence: true}
   
   def user
     return User.find_by(id: self.user_id)
