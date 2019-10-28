b99c2b1a827d878ad2ae39d5f1277e8c63f92342
Tomoyuki Oka <tomochiki00@yahoo.co.jp>
2019-10-28 19:12:32 +0000

    Fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_current_user
   def set_current_user
-    @current_user = User.find_by(
-      id: session[:user_id]
-      )
+    @current_user = User.find_by(id: session[:user_id])
   end
   def authenticate_user
     if @current_user == nil
