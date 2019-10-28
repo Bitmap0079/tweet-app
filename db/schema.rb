e0ac66f37a31c00f5e713a38b79beba3fece9c3d
Tomoyuki Oka <tomochiki00@yahoo.co.jp>
2019-10-28 19:00:16 +0000

    change db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191020154254) do
+ActiveRecord::Schema.define(version: 20191028185828) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20191020154254) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.string "user_name"
+    t.string "title"
   end
 
   create_table "users", force: :cascade do |t|
